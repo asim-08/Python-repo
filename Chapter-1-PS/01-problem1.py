cbc67af512395731f55a9222331818f5534b9b90
asim-08 <asimkhanwc786@gmail.com>
2025-06-16 00:43:17 +0500

    pushing chapter 1

--- Chapter-1-PS/01-problem1.py
+++ Chapter-1-PS/01-problem1.py
@@ -1,2 +1,1 @@
-poem = "Twinkle twinkle little star.How I wonder what you are.Up above the world so high.Like a diamond in the sky.Twinkle twinkle little star.How I wonder what you are.Twinkle twinkle little star.How I wonder what you are.Up above the world so high.Like a diamond in the sky.Twinkle twinkle little star.How I wonder what you are."
-print(poem)
+print("Twinkle twinkle little star.How I wonder what you are.Up above the world so high.Like a diamond in the sky.Twinkle twinkle little star.How I wonder what you are.Twinkle twinkle little star.How I wonder what you are.Up above the world so high.Like a diamond in the sky.Twinkle twinkle little star.How I wonder what you are.")
