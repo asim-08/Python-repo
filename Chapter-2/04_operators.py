81fb16a5972fe6600310b7c84d0fda631b10e65d
asim-08 <asimkhanwc786@gmail.com>
2025-06-17 00:57:13 +0500

    pushing some changes in my code

--- Chapter-2/04_operators.py
+++ Chapter-2/04_operators.py
@@ -17,14 +17,13 @@ a = 5>4
 print(a)
 
 #Logical operators
-# Truth table of or
+
 e = True or False
+# Truth table of or
 print("True or False is", True or False)
 print("False or True is", True or False)
 print("True or True is", True or False)
 print("False or False is", False or False)
-
-
 
 # Truth table of and
 print("True and False is", True and False)
