2afe6763e3b3ae403a65dcc12d437a5924ebced2
asim-08 <asimkhanwc786@gmail.com>
2025-06-16 22:53:15 +0500

    pushing chapter-2

---
+++ Chapter-2/02_datatypes.py
@@ -0,0 +1,5 @@
+a = 1        # a is an integer.
+b = 5.22     # b is an floating point number.
+c = "Asim"   # c is a string.
+d = False/True     # d is an boolean variable.
+e = None     # e is an none variable.
