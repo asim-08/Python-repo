2b9ae323502a8abcd9fd3d3d5c6ecac77c18a351
asim-08 <asimkhanwc786@gmail.com>
2025-06-17 00:55:34 +0500

    pushing chapter-2

---
+++ Chapter-2/04_operators.py
@@ -0,0 +1,37 @@
+# Arthmetic operators
+a = 4
+b = 3
+c = a + b
+print(c)
+
+# Assignment operators
+a = 4-2  #Assign 4-2 in a.
+a += 3   #It means increment the value of a by 3.
+print(a)
+b = 3-4  #Assigns 3-4 in b.
+b *= -4
+print(b)
+
+# Comparison operator
+a = 5>4
+print(a)
+
+#Logical operators
+# Truth table of or
+e = True or False
+print("True or False is", True or False)
+print("False or True is", True or False)
+print("True or True is", True or False)
+print("False or False is", False or False)
+
+
+
+# Truth table of and
+print("True and False is", True and False)
+print("False and True is", True and False)
+print("True and True is", True and True)
+print("False and False is", False and False)
+
+# Truth table of not
+print(not(True))
+print(not(False))
