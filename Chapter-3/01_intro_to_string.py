d6a6d3fe3940c34e182e714b897f44a3381812ff
asim-08 <asimkhanwc786@gmail.com>
2025-06-18 23:02:38 +0500

    Adding chapter-3

---
+++ Chapter-3/01_intro_to_string.py
@@ -0,0 +1,9 @@
+name = "Asim"
+nameshort = name[0:3] # Name starts from index 0 all the way till 3 (excluding 3)
+print(nameshort)
+character = name[1]
+print(character)
+# print(name.upper())
+# print(name.lower())
+# print(len(name))
+# print(name[0])
