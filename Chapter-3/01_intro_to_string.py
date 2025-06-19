37ab63c90fecfa8eef57bf47e155fc0570082625
asim-08 <asimkhanwc786@gmail.com>
2025-06-19 12:37:10 +0500

    adding chapter-3

--- Chapter-3/01_intro_to_string.py
+++ Chapter-3/01_intro_to_string.py
@@ -3,7 +3,4 @@ nameshort = name[0:3] # Name starts from index 0 all the way till 3 (excluding 3
 print(nameshort)
 character = name[1]
 print(character)
-# print(name.upper())
-# print(name.lower())
-# print(len(name))
-# print(name[0])
+
---
+++ Chapter-3/04_str-funtions.py
@@ -0,0 +1,13 @@
+name = "Asim"
+print(len(name))
+print(name.upper())
+print(name.lower())
+print(name.endswith("im"))
+print(name.startswith("A"))
+print(name.capitalize())
+
+
+
+
+name = "Asim is a good boy"
+print(name.replace("good" , "bad"))
