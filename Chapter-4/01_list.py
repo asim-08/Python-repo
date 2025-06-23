a82da3ba51194d7301364db1f4fd99903c025d93
asim-08 <asimkhanwc786@gmail.com>
2025-06-23 13:05:25 +0500

    learning about list

---
+++ Chapter-4/01_list.py
@@ -0,0 +1,5 @@
+friends = ["Apple", "orange", 32, 122.34, "Asim", "sudais"]
+print(friends[1])
+friends[0] = "Grapes"                 # Lists are mutable.
+print(friends)
+print(friends[1:4])
---
+++ Chapter-4/02_list_methods.py
@@ -0,0 +1,10 @@
+friends = ["Apple", "orange", 32, 122.34, "Saim", "sudais"]
+print(friends)
+friends.append("Asim")
+print(friends)
+l1 = [1, 2, 65, 33, 2, 23]
+# l1.sort()
+# l1.reverse()
+# l1.insert(3, 3333) # It will insert 3333 at index 3.
+l1.pop(2)            # It will delete the value which is at the index that you give.
+print(l1)
