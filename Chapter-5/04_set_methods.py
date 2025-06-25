bbf43a125abd8301159eb73c54c5bc438fb14199
asim-08 <asimkhanwc786@gmail.com>
2025-06-26 01:50:32 +0500

    Learning about set methods

--- Chapter-5/04_set_methods.py
+++ Chapter-5/04_set_methods.py
@@ -1,3 +1,28 @@
 s = {1, 5, 32, 55, 5, "Asim", "Asim"}
 
+#  s.add(9990)    	# Adds a single item.
+#  print(s)
 
+#  s.remove(5)       # Removes an item, gives error if not found.
+#  print(s)
+
+#  s.discard(1)      # Removes item, no error if not found.
+#  print(s)
+
+#  s.clear()         # Removes all items from the set.
+#  print(s)
+
+# s.pop()           # Removes a random item.
+# print(s)
+
+# s. copy()           # Returns a copy of the set
+# print(s)
+
+
+'''
+These all line are line of code.
+but it give some error that has just one solution.
+need to remove all methods and run it one by one.
+Thats why i comment out these all line.
+
+'''
