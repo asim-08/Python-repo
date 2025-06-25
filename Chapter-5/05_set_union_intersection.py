43274bb7b2f227fcbe596022f795ef37f3716edb
asim-08 <asimkhanwc786@gmail.com>
2025-06-26 02:08:58 +0500

    Learning about union and intersection

---
+++ Chapter-5/05_set_union_intersection.py
@@ -0,0 +1,8 @@
+s1 = {1, 2, 3, 4, 5}
+s2 = {1, 5, 6, 7, 8}
+print(s1.union(s2))
+print(s1.intersection(s2))
+print(s1.issubset(s2))
+print(s1.issuperset(s2))
+print(s1.isdisjoint(s2))
+print(s1.difference(s2))
