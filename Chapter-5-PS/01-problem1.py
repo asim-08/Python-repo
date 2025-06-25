339ad3c9c7fefecd04edeeaeffbd945a72a9e0cb
asim-08 <asimkhanwc786@gmail.com>
2025-06-26 03:08:55 +0500

    Doing some problems by my own

---
+++ Chapter-5-PS/01-problem1.py
@@ -0,0 +1,7 @@
+d = {
+    "madad": "Help",
+    "kursi": "chair",
+    "billi": "cat"
+}
+words = input("Enter the word you want meaning of: ")
+print(d[words])
---
+++ Chapter-5-PS/06-problem-.py
@@ -0,0 +1,19 @@
+d = {}
+
+name = input("Enter you'r friend name: ")
+lang = input("Enter you'r language name: ")
+d.update({name: lang})
+
+name = input("Enter you'r friend name: ")
+lang = input("Enter you'r language name: ")
+d.update({name: lang})
+
+name = input("Enter you'r friend name: ")
+lang = input("Enter you'r language name: ")
+d.update({name: lang})
+
+name = input("Enter you'r friend name: ")
+lang = input("Enter you'r language name: ")
+d.update({name: lang})
+
+print(d)
