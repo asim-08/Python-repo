839f10ce0ad709060463932002931159a44f5fa1
asim-08 <asimkhanwc786@gmail.com>
2025-06-29 00:09:43 +0500

    Doing some changes

--- Chapter-6/01_conditionals.py
+++ Chapter-6/01_conditionals.py
@@ -3,13 +3,7 @@ age = int(input("Enter you'r age: "))
 if (age>18):
     print("You are above the age of concent")
     print("So You can vote")
-
-elif (age<0):
-    print("You are entering an Negative invalid age")
-
-elif(age==0):
-    print("You are entering 0 which is invalid age")
-
+    
 else:
     print("You are below the age of concent")
     print("So You can not vote")
