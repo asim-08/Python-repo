668416cab5243fc827a99bfc52ebbd2db56f138b
asim-08 <asimkhanwc786@gmail.com>
2025-06-28 23:51:16 +0500

    Learning about conditional statement

---
+++ Chapter-6/01_conditionals.py
@@ -0,0 +1,8 @@
+age = int(input("Enter you'r age: "))
+if (age>18):
+    print("You are above the age of concent")
+    print("So You can vote")
+else:
+    print("You are below the age of concent")
+    print("So You can not vote")
+print("End of program")
