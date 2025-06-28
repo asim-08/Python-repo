5e80a56a35e1dd8f6833732fa4fa93e9fca050a4
asim-08 <asimkhanwc786@gmail.com>
2025-06-29 00:24:23 +0500

    Doing some changes

--- Chapter-6/01_conditionals.py
+++ Chapter-6/01_conditionals.py
@@ -1,9 +1,9 @@
 age = int(input("Enter you'r age: "))
-# "if" "elif" "else" ladder.
+# if else statement.
 if (age>18):
     print("You are above the age of concent")
     print("So You can vote")
-    
+
 else:
     print("You are below the age of concent")
     print("So You can not vote")
