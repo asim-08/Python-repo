5e7f3a5cd6db67d9cd31deb246bd343b7efe58e4
asim-08 <asimkhanwc786@gmail.com>
2025-07-02 16:08:33 +0500

    Learning about iteration using for loop

---
+++ Chapter-7/06_for_loop_iterate.py
@@ -0,0 +1,13 @@
+# for loop with list.
+l = [1, "Asim", 231.43, False, 45]
+for i in l:
+    print(i)
+# for loop with tuple.
+t = (6, 7, 55, 334, 33.4, "Asim")
+print(type(t))
+for i in t:
+    print(i)
+# for loop with string.
+s = "Asim"
+for i in s:
+    print(i)
