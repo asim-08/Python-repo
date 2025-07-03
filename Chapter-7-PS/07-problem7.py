878c69152771cc13a586066bd5472aede7c2d76b
asim-08 <asimkhanwc786@gmail.com>
2025-07-03 12:19:13 +0500

    some changes in code

--- Chapter-7-PS/07-problem7.py
+++ Chapter-7-PS/07-problem7.py
@@ -6,7 +6,11 @@ for n = 3
 '''
 
 n = int(input("Enter the number: "))
-for i in range(1, n+1):
-    print(" "* (n-i), end="")
-    print("*"* (2*i-1), end="")
-    print("")
+for i in range(n):
+    for j in range (i, n):
+        print(" ", end = "")
+    for j in range (i):
+        print("*", end = "")
+    for j in range (i + 1):
+        print("*", end = "")
+    print()
--- Chapter-7-PS/08-problem8.py
+++ Chapter-7-PS/08-problem8.py
@@ -1,4 +1,9 @@
 n = int(input("Enter the number: "))
-for i in range(1, n+1): 
-    print("*"* i, end="")
-    print("")
+for i in range (n):
+    for j in range (i):
+        print("", end = "")
+    for j in range (i + 1):
+        print("*", end = " ")
+    
+
+    print()
