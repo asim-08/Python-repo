5cfcd7c10651ecce937b8950f5929da9d9867a0a
asim-08 <asimkhanwc786@gmail.com>
2025-07-07 15:42:26 +0500

    Learning about default value

---
+++ Chapter-8/04_Default_with_argument.py
@@ -0,0 +1,5 @@
+def greet(name, ending = "Thank you"):
+    print(F"Good day, {name}")
+    print(ending)
+greet("Asim")
+greet("Muzamil", "thanks")
---
+++ Test-9/test.py
@@ -0,0 +1,8 @@
+a = 100
+b = 901
+a = (a + b) - a
+print(a)
+b = (100 / b) * b
+print(b)
+
+
